fae238c07fecd8a08953082e085760058d34cfb0
jmeza <dart.bekicfox@gmail.com>
2019-05-05 20:23:46 -0500

    cambios

--- client/src/app/pages/comparativa/comparativa.component.ts
+++ client/src/app/pages/comparativa/comparativa.component.ts
@@ -51,7 +51,6 @@ export class ComparativaComponent implements OnInit, OnDestroy {
 			this.tastes = res;
 		});
 		this.sub_data_sabores = this.app_srv.beneficio_x_mascota.pipe(filter(fil => fil != null)).subscribe(res => {
-			console.log(res);
 			this.benefice = res;
 		});
 	}
@@ -132,7 +131,6 @@ export class ComparativaComponent implements OnInit, OnDestroy {
 		}
 		this.filterData = null;
 		this.app_srv.getDataScraperBySite(obj).subscribe(res => {
-			console.log('res => ',res);
 			this.filterData  = res;
 		});
 	}
