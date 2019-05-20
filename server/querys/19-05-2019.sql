30dbfcdd6af597cbf4a4d582d937a674384bf581
Ancaor <ancaors1@gmail.com>
2019-05-19 19:49:36 -0500

    filtros

--- server/querys/19-05-2019.sql
+++ server/querys/19-05-2019.sql
@@ -13,10 +13,8 @@ WITH (
 
 INSERT INTO recomendacion (desc_recomendacion, img_recomendacion, class_recomendacion, id_sec_recomedacion)
                    VALUES ('Sabor'           , 'sabor.png'      , 'card__top'        , 2),
-				          ('Beneficios'      , 'beneficios.png' , ''                 , 3),
-						  ('Costos'          , 'money.png'      , 'card__bottom'     , 4);
-
-
+													('Beneficios'      , 'beneficios.png' , ''                 , 3),
+													('Costos'          , 'money.png'      , 'card__bottom'     , 4);
 
 ALTER TABLE public.alimento
  ADD COLUMN tags CHARACTER VARYING[];
@@ -106,7 +104,7 @@ BEGIN
 			SELECT *
 	          FROM filtro_4
              WHERE TRUE = ((SELECT __func_02_exist_value_in_tags(LOWER(nombre), __tag_size)) OR (SELECT __func_02_exist_value_in_tags(LOWER(descripcion), __tag_size)))
-		    
+
 	   ), datos_filtrados AS(
 			SELECT *,
 				  (SELECT __get_arry_nombre_mark(LOWER(s.nombre),(SELECT ARRAY_AGG(LOWER(tab.val))::CHARACTER VARYING[]
@@ -153,6 +151,117 @@ BEGIN
 		          'webs'   , COALESCE((SELECT * FROM webs), '{}'::TEXT[])
 	        )
 	   INTO __result;
+
+    RETURN __result;
+EXCEPTION
+    WHEN SQLSTATE 'MASCO' THEN
+        RETURN JSONB_BUILD_OBJECT('status', 400, 'msj', SQLERRM);
+    WHEN OTHERS THEN
+        GET STACKED DIAGNOSTICS __msj_excep = PG_EXCEPTION_CONTEXT;
+        RETURN JSONB_BUILD_OBJECT('status', 500, 'msj', 'Hubo un error desconocido.', 'stack_error', CONCAT(SQLERRM, ' ', __msj_excep));
+END;
+
+$BODY$;
+
+INSERT INTO persona (id_persona, nom_persona, ape_pate_pers, flg_acti)
+SELECT 0, 'Invitado', 'Desconocido' , '1';
+
+CREATE TABLE public.persona_x_filtro
+(
+    id serial NOT NULL,
+    _id_animal integer NOT NULL,
+    _id_recomendacion integer NOT NULL,
+    _id_correlativo_raza integer,
+    _id_correlativo_size integer,
+    _sabores integer[],
+    _beneficios integer[],
+    _id_correlativo_edad integer,
+    _id_persona integer NOT NULL,
+    CONSTRAINT persona_x_animal FOREIGN KEY (_id_animal)
+        REFERENCES public.animal (id_animal) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT persona_x_recomedacion FOREIGN KEY (_id_recomendacion)
+        REFERENCES public.recomendacion (id_recomendacion) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT persona_x_raza FOREIGN KEY (_id_animal,_id_correlativo_raza)
+        REFERENCES public.raza (_id_animal,correlativo) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT persona_x_size FOREIGN KEY (_id_animal,_id_correlativo_size)
+        REFERENCES public.size (_id_animal,correlativo) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT persona_x_edad FOREIGN KEY (_id_animal,_id_correlativo_edad)
+        REFERENCES public.edades (_id_animal,correlativo) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT persona FOREIGN KEY (_id_persona)
+        REFERENCES public.persona (id_persona) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+);
+
+CREATE OR REPLACE FUNCTION public.__func_02_save_data_user(
+	__id_mascota integer,
+	__recomendacion integer,
+	__filtros jsonb,
+	__id_user integer)
+    RETURNS jsonb
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE
+AS $BODY$
+
+DECLARE
+    --CONSTANTES
+		__FLG_ACTI        CHARACTER VARYING DEFAULT '1';
+		__FLG_INACTI      CHARACTER VARYING DEFAULT '0';
+		__RECOM_SABOR     INTEGER           DEFAULT 1;
+		__RECOM_BENEFICIO INTEGER           DEFAULT 2;
+		__RECOM_COSTO     INTEGER           DEFAULT 3;
+
+    --VARIABLES
+		__result              JSONB;
+		__msj_excep           TEXT;
+
+		__id_recomendacion  INTEGER;
+		__id_raza           INTEGER;
+		__edad 				INTEGER;
+		__id_age			INTEGER;
+		__id_size			INTEGER;
+		__sabores			INTEGER[] = null;
+		__beneficios		INTEGER[] = null;
+		__selected  		CHARACTER VARYING;
+BEGIN
+	__id_raza 	:= (__filtros->>'race')::INTEGER;
+	__id_age  	:= (__filtros->>'age')::INTEGER;
+	__id_size 	:= (__filtros->>'size')::INTEGER;
+	__selected 	:= (__filtros->>'selected');
+
+	SELECT id_recomendacion
+	  INTO __id_recomendacion
+	  FROM recomendacion
+	 WHERE id_sec_recomedacion = __recomendacion;
+
+	IF __id_recomendacion = __RECOM_SABOR THEN
+		SELECT ARRAY_AGG(id_sabor)
+	  	  INTO __sabores
+	      FROM sabor WHERE abvr IN (SELECT UNNEST(STRING_TO_ARRAY(__selected, ',')));
+	ELSIF __id_recomendacion = __RECOM_BENEFICIO THEN
+		SELECT ARRAY_AGG(id_beneficio)
+	  	  INTO __beneficios
+	      FROM beneficio WHERE abvr IN (SELECT UNNEST(STRING_TO_ARRAY(__selected, ',')));
+	END IF;
+
+	INSERT INTO persona_x_filtro (_id_animal  , _id_recomendacion , _id_correlativo_raza, _id_correlativo_size, _id_correlativo_edad, _sabores , _beneficios , _id_persona)
+		                  VALUES (__id_mascota, __id_recomendacion, __id_raza           , __id_size           , __id_age            , __sabores, __beneficios, COALESCE(__id_user, 0) );
+	__result := JSONB_BUILD_OBJECT('status', 200);
 
     RETURN __result;
 EXCEPTION
